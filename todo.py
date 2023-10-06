88e80c2f9b18153d65cc8b2954d4bd85811d49fb
Aryans <quispeloayza18@hotmail.com>
2023-10-05 21:38:58 -0500

    Page Layout

--- todo.py
+++ todo.py
@@ -1,14 +1,31 @@
 import flet as ft
 
+
 def main(page: ft.Page):
 
     def add_task(e):
-        page.add(ft.Checkbox(label=new_task.value))
-        new_task.value=''
+        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
+        new_task.value = ''
         page.update()
 
-    new_task=(ft.TextField(hint_text="Escribe una tarea"))
+    new_task = (ft.TextField(hint_text="Escribe una tarea", expand=True))
+    tasks_view = ft.Column()
+    view = ft.Column(
+        width=600,
+        controls=[
+            ft.Row(
+                controls=[
+                    new_task, ft.FloatingActionButton(
+                        icon=ft.icons.ADD, on_click=add_task),
+                ],
+            ),
 
-    page.add(new_task,ft.FloatingActionButton(icon=ft.icons.ADD,on_click=add_task))
+            tasks_view,
+        ],
+
+    )
+
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(view)
 
 ft.app(target=main)
