5c4daf25c7df96b6a8df538eaaabe9903738b6a3
Aryans <quispeloayza18@hotmail.com>
2023-10-08 23:41:46 -0500

    Reusable UI components

--- todo.py
+++ todo.py
@@ -1,31 +1,40 @@
 import flet as ft
 
 
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(
+            hint_text="Escribe una tarea", expand=True)
+        self.tasks = ft.Column()
+
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task, ft.FloatingActionButton(
+                            icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+
+                self.tasks,
+            ]
+        )
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ''
+        self.update()
+
+
 def main(page: ft.Page):
-
-    def add_task(e):
-        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-        new_task.value = ''
-        page.update()
-
-    new_task = (ft.TextField(hint_text="Escribe una tarea", expand=True))
-    tasks_view = ft.Column()
-    view = ft.Column(
-        width=600,
-        controls=[
-            ft.Row(
-                controls=[
-                    new_task, ft.FloatingActionButton(
-                        icon=ft.icons.ADD, on_click=add_task),
-                ],
-            ),
-
-            tasks_view,
-        ],
-
-    )
-
+    page.title = "ToDo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
+
+    todo = TodoApp()
+
+    page.add(todo)
+
 
 ft.app(target=main)
